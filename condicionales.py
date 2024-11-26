e012da1467f5f371ce1d46f297b4d8a29cef9878
Miaf03 <mia03luna@gmail.com>
2024-11-26 17:42:19 -0600

    Actualizaciones

--- condicionales.py
+++ condicionales.py
@@ -3,28 +3,46 @@
 
 edad = int(input("Ingresa tu edad: "))
 
-if edad <= 17:
-    print("Eres menor de edad")
+if edad >= 18:
+    print("Eres mayor de edad")
 else:
-    print("Eres mayor de edad") 
+    print("Eres menor de edad")
 
-# Ejercicio 2. Escribe un programa que almacene la cadena 'contraseña' en una variable, pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
+# Ejercicio 2. Escribe un programa que almacene la cadena 'contraseña' en una variable, luego pregunte al usuario por la contraseña e imprima por pantalla si la contraseña introducida por el usuario coincide con la guardada en la variable sin tener en cuenta mayúsculas y minúsculas.
 
 key = "contraseña"
 password = input("Ingresa la contraseña: ")
 
 if key == password.lower():
-    print("La contraseña coincide")
+    print("Contraseña correcta")
 else: 
-    print("La contraseña no coincide")
+    print("Contraseña incorrecta")
 
 # Ejercicio 3. Escribe un programa que pida al usuario dos números y muestre por pantalla su división. Si el divisor es cero el programa debe mostrar un error.
 
-n1 = float(input("Introduce el dividendo: "))
-n2 = float(input("Introduce el divisor: "))
+n = float(input("Ingresa el numerador: "))
+m = float(input("Ingresa el denominador: "))
 
-if n2 == 0:
-    print("¡Error! No se puede dividir por 0")
+if m == 0:
+    print("¡error!, no se puede dividir entre cero")
 else:
-    print(f"El resultado de la división es: {n1 / n2:.2f}")
+    print(f"El resultado es: {n / m:.2f}")
     
+# Ejercicio 4. Escribe un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+
+numero = int(input("Ingresa un número entero: "))
+
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
+
+# Ejercicio 5. Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos ingresos iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene que tributar o no.
+
+edad = int(input("Ingresa tu edad: "))
+ingresos = float(input("Agrega tus ingresos mensuales en euros: "))
+
+if edad > 16 and ingresos >= 1000:
+    print("Tienes que tributar")
+else: 
+    print("No tienes que tributar")
