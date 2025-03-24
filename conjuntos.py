e1e0738b823687e446a5eb0e0a9b55ff279d1aa4
Miaf03 <mia03luna@gmail.com>
2025-03-24 11:50:54 -0600

    Nuevas Actualizaciones

--- conjuntos.py
+++ conjuntos.py
@@ -1,4 +1,4 @@
-# Crea un conjunto con 5 números, agrega un nuevo número al conjunto e imprímelo, elimina un número del conjunto e imprímelo
+# Crea un conjunto con 5 números, agrega un nuevo número y  elimina otro número e imprímelo
 
 conjunto = {2, 5, 7, 3, 9}
 print(conjunto)
@@ -9,7 +9,7 @@ print(conjunto)
 conjunto.remove(2)
 print(conjunto)
 
-# Declara dos conjuntos con diferentes animales. Une ambos conjuntos en uno solo e imprimelo
+# Declara dos conjuntos con diferentes animales. Une ambos conjuntos en uno solo
 
 a = {"leon", "tortuga", "pajaro"}
 b = {"raton", "cucaracha", "mariposa"}
@@ -18,25 +18,19 @@ union = a | b
 print(union)
 
 
-# Declara dos conjuntos con colores (algunos en común). Encuentra los colores que tienen en común e imprímelos.
+# Declara dos conjuntos con colores (algunos en común encuentra los colores que tienen en común e imprímelos
 
-colores_a = {"azul", "morado", "naranja", "rosa"}
-colores_b = {"verde", "rosa", "rojo", "turquesa"}
+a = {"azul", "morado", "naranja", "rosa"}
+b = {"verde", "rosa", "rojo", "turquesa"}
 
-colores_comunes =  colores_a & colores_b
+colores_comunes =  a & b
 print(colores_comunes)
 
 
-# Declara dos conjuntos con algunos números en común. Muestra los elementos que están en el primer conjunto pero no en el segundo.
+# Declara dos conjuntos con algunos números en común. Muestra los elementos que están en el primer conjunto pero no en el segundo
 
 numero_1 = {1, 3, 5, 6, 7, 8}
 numero_2 = {1, 2, 4, 5, 6, 9}
 
 diferencia = numero_1 - numero_2
 print(diferencia)
-
-# Crea una lista con palabras repetidas. Convierte la lista en un conjunto para eliminar duplicados. Imprime el resultado.
-
-lista = ["memo", "mia", "memo", "mario", "memo"]
-lista = set(lista)
-print(lista)
--- tuplas.py
+++ tuplas.py
@@ -1,18 +1,18 @@
 # Escribe un programa que recorra una tupla e imprima cada uno de sus elementos en mayúsculas
 
-ciudades = ("cuernavaca", "cdmx", "villahermosa", "huitziltepec")
+ciudades = ("Cuernavaca", "CDMX", "Villahermosa", "Guadalajara")
 
 for ciudad in ciudades:
     print(ciudad.upper())
     
-# Crea una tupla con 5 nombres, imprime el primer y el último nombre de la tupla
+# Crea una tupla con 5 nombres e imprime el primer y el último nombre de la tupla
 
 nombres = ("Uriel", "Yajaira", "Yian", "Dayra", "Yonathan")
 
 print(nombres[0])
 print(nombres[-1])
 
-# Crea 2 tuplas una con frutas y otra con verduras. Une ambas tuplas en una nueva tupla e imprimela 
+# Crea 2 tuplas una con frutas y otra con verduras y une ambas en una nueva tupla
 
 frutas = ("Manzana", "Mango", "Mandarina")
 verduras =  ("Chile", "Lechuga", "Aguacate")
@@ -20,14 +20,9 @@ verduras =  ("Chile", "Lechuga", "Aguacate")
 union = frutas + verduras
 print(union)
 
-# Crea una tupla con varios números repetidos. Usa .count() para contar cuántas veces aparece un número especifico 
-
-numeros = (1, 3, 1, 5, 3, 6, 8, 4, 7, 3, 1)
-print(numeros.count(1))
-
 # Crea una lista con 5 elementos. Convierte la lista en una tupla e imprimela
 
-elementos = ["hidrogeno", "helio", "carbono", "nitrogeno", "oxigeno"]
+elementos = ["hidrogeno", "helio", "litio", "berilio", "boro"]
 tupla = tuple(elementos)
 
 print(tupla)
